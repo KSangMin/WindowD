b8fa10aea4cac3c13d5eb0065a65bfb6322d6d77
KSangMin <fhfpszmfnwm@naver.com>
2025-03-07 11:46:50 +0900

    [Fix] 점프대 충돌 판정 2번 일어나는 버그 수정

--- Assets/Scripts/Map/Obstacles/Jumper.cs
+++ Assets/Scripts/Map/Obstacles/Jumper.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class Jumper : MonoBehaviour
 {
     public float jumpPower = 160f;
+    bool hasJumped;
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (!hasJumped && collision.gameObject.CompareTag("Player"))
         {
             foreach (ContactPoint point in collision.contacts)
             {
@@ -15,13 +16,24 @@ public class Jumper : MonoBehaviour
                 if (cos < -0.5f)
                 {
                     Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+                    rb.velocity = Vector3.zero;
                     rb.AddForce(transform.up * jumpPower, ForceMode.Impulse);
                     Debug.Log("มกวมวิ");
+                    hasJumped = true;
                     break;
                 }
             }
             //Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
             //rb.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hasJumped = false;
         }
     }
 }
